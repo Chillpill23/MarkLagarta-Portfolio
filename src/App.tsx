a4ef7cf64b66c6c24f6c87bf73fc6d177101e5b5
Mark Lagarta <mj,lagarta00@gmail.com>
2022-07-21 04:47:36 +0800

    Bug Fixes for White page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Home from './components/Pages/Home';
 import Navbar from './components/Js/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
       <div className='app-wrapper'>
+        <Router>
           <Navbar />
-          <Home />
+          <Routes>
+            <Route path='/MarkLagarta-Portfolio/' element={<Home/>} />
+          </Routes>
+        </Router>
       </div>
     </>
   );
