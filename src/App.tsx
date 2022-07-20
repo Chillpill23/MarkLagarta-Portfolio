a1a649ce82f3969d30625eb885aca8665ced14ef
Mark Lagarta <mj,lagarta00@gmail.com>
2022-07-21 04:35:14 +0800

    White Page Bug Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Home from './components/Pages/Home';
 import Navbar from './components/Js/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
       <div className='app-wrapper'>
-        <Router>
           <Navbar />
-          <Routes>
-            <Route path='/' element={<Home/>} />
-          </Routes>
-        </Router>
+          <Home />
       </div>
     </>
   );
